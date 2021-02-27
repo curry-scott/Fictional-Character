b3bd3789cc1d6f206ecf029cca58445ce8fa97d9
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-27 11:39:23 -0500

    update

--- js/main.js
+++ js/main.js
@@ -2,32 +2,30 @@ document.querySelectorAll('.form-outline').forEach((formOutline) => {
     new mdb.Input(formOutline).update();
   });
 
-  //Setting date to countdown to
-const countDownDate = new Date("Jan 01, 2022 00:00:00").getTime();
+  
+const countDownDate = new Date("Nov 05, 2021 00:00:00").getTime();
 
-//Countdown by seconds to mins to hours to days
+
 let myfunc = setInterval(function() {
     let now = new Date().getTime();
     let timeleft = countDownDate - now;
 
-    // Setting days, hours, mins and secs to go 
-    // 1000 milliseconds, 60 secs, 60 mins, 24 hours 
+    
     let { days, hours, minutes, seconds } = newFunction(timeleft);
 
-    //Displaying days, hours, mins and secs
+    
     document.getElementById("days").innerHTML = days + "d "        
     document.getElementById("hours").innerHTML = hours + "h " 
     document.getElementById("mins").innerHTML = minutes + "m " 
     document.getElementById("secs").innerHTML = seconds + "s " 
 
-    //Displaying countdown ending message
     if (timeleft < 0) {
         clearInterval(myfunc);
         document.getElementById("days").innerHTML = ""
         document.getElementById("hours").innerHTML = "" 
         document.getElementById("mins").innerHTML = ""
         document.getElementById("secs").innerHTML = ""
-        document.getElementById("end").innerHTML = "!!HAPPY NEW YEAR!!";
+        document.getElementById("end").innerHTML = "!!VOTE FOR JAMES BOND!";
     }
 }, 
 1000);
